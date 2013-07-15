fab9b6d9b6cf31a7043480f4c463331831120d51
Aidan Wood <fenway1000@gmail.com>
2013-07-15 09:49:58 -0700

    Change text a bit

--- scoreinput.php
+++ scoreinput.php
@@ -7,22 +7,22 @@
 
  <form action="scoreboard.php" method="post">
  <b>Great White Shark--></b>
- Amount to Be Added: <input type="text" name="score1">
+Number of Stars: <input type="text" name="score1">
  <br>
 <b>Polar Bear--></b>
- Amount to Be Added: <input type="text" name="score2">
+ Number of Stars: <input type="text" name="score2">
  <br>
 <b>Honey Badger--></b>
- Amount to Be Added: <input type="text" name="score3">
+ Number of Stars: <input type="text" name="score3">
  <br>
 <b>Wolverine--></b>
- Amount to Be Added: <input ="text" name="score4">
+ Number of Stars: <input ="text" name="score4">
  <br>
 <b>Blue Whale--></b>
- Amount to Be Added: <input type="text" name="score5">
+ Number of Stars: <input type="text" name="score5">
  <br>
 <b>Fire Ant Colony--></b>
- Amount to Be Added: <input type="text" name="score6">
+ Number of Stars: <input type="text" name="score6">
  <br>
  <b>Password:</b> <input type="password" id="password" name="password">
  <br>
